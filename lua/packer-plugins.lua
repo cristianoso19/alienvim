43df2d066d39dcbd18607eeb361a97f58aec08a3
Cristian Fedora <cristianoso19@gmail.com>
2022-11-12 18:40:34 -0500

    Rebuild configuration

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -1,97 +1,84 @@
 return require ('packer').startup(function()
-	-- packer can manage itself
-	use 'wbthomason/packer.nvim'
-	-- colorscheme 
-	use 'folke/tokyonight.nvim'
-
-
-	require("tokyonight").setup({
-		style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-		transparent = true, -- Enable this to disable setting the background color
-		on_highlights = function(hl, c)
-			local prompt = "#2d3149"
-			hl.TelescopeNormal = {
-				bg = c.bg_dark,
-				fg = c.fg_dark,
-			}
-			hl.TelescopeBorder = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-			hl.TelescopePromptNormal = {
-				bg = prompt,
-			}
-			hl.TelescopePromptBorder = {
-				bg = prompt,
-				fg = prompt,
-			}
-			hl.TelescopePromptTitle = {
-				bg = prompt,
-				fg = prompt,
-			}
-			hl.TelescopePreviewTitle = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-			hl.TelescopeResultsTitle = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-		end,
-	})
-	use "nvim-lua/plenary.nvim"
-
 	use {
+		-- packer can manage itself
+		'wbthomason/packer.nvim',
+		-- colorscheme 
+		'folke/tokyonight.nvim',
+		'lukas-reineke/indent-blankline.nvim',
+		'nvim-lua/plenary.nvim',
+		'glepnir/dashboard-nvim',
+		'xiyaowong/telescope-emoji.nvim',
+		'folke/lsp-colors.nvim',
+
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		'nvim-telescope/telescope-fzf-native.nvim', run = 'make',
 		'nvim-telescope/telescope-file-browser.nvim',
-		-- or                            , branch = '0.1.x',
-		require('telescope').setup {
-			extensions = {
-				fzf = {
-					fuzzy = true,                    -- false will only do exact matching
-					override_generic_sorter = true,  -- override the generic sorter
-					override_file_sorter = true,     -- override the file sorter
-					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-				}
+
+
+		'nvim-lualine/lualine.nvim',
+		'kyazdani42/nvim-web-devicons',
+		'folke/which-key.nvim',
+		'phaazon/hop.nvim', branch = 'v2',
+		'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
+		'nvim-treesitter/nvim-treesitter-textobjects',
+
+		'p00f/nvim-ts-rainbow', 
+
+		'folke/trouble.nvim',
+		'akinsho/bufferline.nvim', tag = "v3.*",
+		'm4xshen/autoclose.nvim',
+		'preservim/nerdcommenter',
+		'neovim/nvim-lspconfig',
+		'williamboman/nvim-lsp-installer',
+		'yamatsum/nvim-cursorline',
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-nvim-lsp',
+		'saadparwaiz1/cmp_luasnip',	
+		'L3MON4D3/LuaSnip',
+		'folke/twilight.nvim',
+		'folke/zen-mode.nvim',
+		'folke/todo-comments.nvim',
+		'kevinhwang91/nvim-hlslens',
+		'petertriho/nvim-scrollbar',
+		'neoclide/coc.nvim', branch = 'master', run = 'npm install --frozen-lockfile',
+		'yaegassy/coc-blade', run = 'npm install --frozen-lockfile',
+		'sheerun/vim-polyglot',
+		'tpope/vim-surround',
+		'mattn/emmet-vim',
+		'karb94/neoscroll.nvim',
+		'tpope/vim-sensible',
+		'windwp/nvim-ts-autotag',
+		'mg979/vim-visual-multi', branch = 'master',
+		'AndrewRadev/tagalong.vim',
+		'SirVer/ultisnips',
+		'mhartington/formatter.nvim',
+		'dcampos/nvim-snippy',
+		'dcampos/cmp-snippy',
+		"windwp/nvim-autopairs",
+		"akinsho/toggleterm.nvim", tag = '*', 
+	}
+	require'hop'.setup {}
+	require("which-key").setup {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	}
+	require("nvim-autopairs").setup {}
+	require('telescope').setup {
+		extensions = {
+			fzf = {
+				fuzzy = true,                    -- false will only do exact matching
+				override_generic_sorter = true,  -- override the generic sorter
+				override_file_sorter = true,     -- override the file sorter
+				case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
 			}
 		}
 	}
-	use 'glepnir/dashboard-nvim'
-	use 'xiyaowong/telescope-emoji.nvim'
-	require("telescope").load_extension "file_browser"
-	require("telescope").load_extension "emoji"
-	use {
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
-		end
+	require("trouble").setup {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
 	}
-	use "lukas-reineke/indent-blankline.nvim"
-
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	
-	use {
-		'phaazon/hop.nvim',
-		branch = 'v2',
-		config = function()
-			require'hop'.setup {}
-		end
-	}
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use {'nvim-treesitter/nvim-treesitter-textobjects'}
-	use 'p00f/nvim-ts-rainbow' 
-
 	require'nvim-treesitter.configs'.setup {
 		indent = {
 			enable = true
@@ -106,43 +93,23 @@ return require ('packer').startup(function()
 		}
 	}
 
-	use {
-	"folke/trouble.nvim",
-	requires = "kyazdani42/nvim-web-devicons",
-	config = function()
-		require("trouble").setup {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
-	end
-	}
 
-	use 'folke/lsp-colors.nvim'
 	require("lsp-colors").setup({
-		Error = "#db4b4b",
-		Warning = "#e0af68",
-		Information = "#0db9d7",
-		Hint = "#10B981"
-	})
-	-- using packer.nvim
-	use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons'}
-	vim.opt.termguicolors = true
+			Error = "#db4b4b",
+			Warning = "#e0af68",
+			Information = "#0db9d7",
+			Hint = "#10B981"
+		})
 	require("bufferline").setup{
 		options = {
 			diagnostics = "nvim_lsp",
 			diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			local icon = level:match("error") and " " or " "
-			return " " .. icon .. count
+				local icon = level:match("error") and " " or " "
+				return " " .. icon .. count
 			end
 		}
 	}
-	use 'm4xshen/autoclose.nvim'
-	use 'preservim/nerdcommenter'
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/nvim-lsp-installer'
 	require("nvim-lsp-installer").setup {}
-	use 'yamatsum/nvim-cursorline'
 	require('nvim-cursorline').setup {
 		cursorline = {
 			enable = true,
@@ -155,37 +122,16 @@ return require ('packer').startup(function()
 			hl = { underline = true },
 		}
 	}
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'saadparwaiz1/cmp_luasnip'	
-	use 'L3MON4D3/LuaSnip'
-	use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-		require("toggleterm").setup({
-			direction='float',
-			open_mapping = '<C-g>',
-		})
-	end}
-	use 'folke/twilight.nvim'
-	use 'folke/zen-mode.nvim'
-	use 'folke/todo-comments.nvim'
+	require("toggleterm").setup{
+		direction='float',
+		open_mapping = '<C-g>',
+	}
 	require('twilight').setup{}
 	require('zen-mode').setup{}
 	require('todo-comments').setup{}
-	use {'kevinhwang91/nvim-hlslens'}
-	require('hlslens').setup({
-		build_position_cb = function(plist, _, _, _)
-			require("scrollbar.handlers.search").handler.show(plist.start_pos)
-		end,
-	})
-	vim.cmd([[
-		augroup scrollbar_search_hide
-			autocmd!
-			autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-		augroup END
-	]])
-	use("petertriho/nvim-scrollbar")
+
 	local colors = require("tokyonight.colors").setup()
-	require("scrollbar").setup({
+	require("scrollbar").setup{
 		handle = {
 			color = colors.bg_highlight,
 		},
@@ -197,31 +143,27 @@ return require ('packer').startup(function()
 			Hint = { color = colors.hint },
 			Misc = { color = colors.purple },
 		}
-	})
-	use {'neoclide/coc.nvim', branch = 'master', run = 'npm install --frozen-lockfile'}
-	use {'yaegassy/coc-blade', run = 'npm install --frozen-lockfile'}
-	use ('sheerun/vim-polyglot')
-	use ('tpope/vim-surround')
-	use ('mattn/emmet-vim')
+	}
+	require('neoscroll').setup{}
+	require('nvim-ts-autotag').setup{}
+	require("formatter").setup{}
+
+	require('hlslens').setup{
+		build_position_cb = function(plist, _, _, _)
+			require("scrollbar.handlers.search").handler.show(plist.start_pos)
+		end,
+	}
+	require("telescope").load_extension "file_browser"
+	require("telescope").load_extension "emoji"
+	-- using packer.nvim
+	vim.opt.termguicolors = true
 	vim.cmd([[
-		let g:user_emmet_leader_key=','
+		augroup scrollbar_search_hide
+			autocmd!
+			autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+		augroup END
+			let g:user_emmet_leader_key=','
 		let g:user_emmet_install_global=0
 		autocmd FileType html,css,vue,blade EmmetInstall
 	]])
-	use 'karb94/neoscroll.nvim'
-	require('neoscroll').setup()
-	use 'tpope/vim-sensible'
-	use {
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
-	use 'windwp/nvim-ts-autotag'
-	require('nvim-ts-autotag').setup()
-	use {'mg979/vim-visual-multi', branch = 'master'}
-	use 'AndrewRadev/tagalong.vim'
-	use 'SirVer/ultisnips'
-	use { 'mhartington/formatter.nvim' }
-	require("formatter").setup()
-	use 'dcampos/nvim-snippy'
-	use 'dcampos/cmp-snippy'
- end)
+end)
